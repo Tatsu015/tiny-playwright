6e47ba4a001fc268a3fbae6cd4f8c0315a34f26d
Tatsu015 <tatsumi.soichiro@gmail.com>
2024-03-16 11:45:27 +0900

    not use zenika image

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,36 @@
-FROM zenika/alpine-chrome:with-node
+FROM alpine:3.19 as base
+
+# Installs latest Chromium package.
+RUN apk upgrade --no-cache --available \
+    && apk add --no-cache \
+    chromium-swiftshader
+
+# Add Chrome as a user
+RUN mkdir -p /usr/src/app \
+    && adduser -D chrome \
+    && chown -R chrome:chrome /usr/src/app
+# Run Chrome as non-privileged
+USER chrome
+WORKDIR /usr/src/app
+
+ENV CHROME_BIN=/usr/bin/chromium-browser \
+    CHROME_PATH=/usr/lib/chromium/
+
+# Autorun chrome headless
+ENV CHROMIUM_FLAGS="--disable-software-rasterizer --disable-dev-shm-usage"
+ENTRYPOINT ["chromium-browser", "--headless"]
+
+FROM base as node
+
+USER root
+RUN apk add --no-cache \
+    tini \
+    nodejs npm
+
+USER chrome
+ENTRYPOINT ["tini", "--"]
+
+FROM node as playwright
 
 ENV PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD 1
 ENV PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH=/usr/bin/chromium-browser
--- src/useragent.js
+++ src/useragent.js
@@ -1,26 +1,13 @@
 const { chromium } = require("playwright-chromium");
 
 (async () => {
-    const { exec } = require("child_process");
-    exec(
-        process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH + " --version",
-        function callback(error, stdout, stderr) {
-            console.log(stdout);
-        }
-    );
-})();
-(async () => {
     const browser = await chromium.launch({
         executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH,
-        // 💡 This enables logs for the communication between Playwright and Chromium
-        // logger: {
-        //   isEnabled: (name, severity) => name === "browser" || "context",
-        //   log: (name, severity, message, args) => console.log(`${name} ${message}`),
-        // },
     });
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto('https://playwright.dev/');
-    await page.screenshot({ path: `example-chromium.png` });
+    const h1 = await page.locator('h1').innerText();
+    console.log('GET BY PLAYWRIGHT:', h1)
     await browser.close();
 })();
